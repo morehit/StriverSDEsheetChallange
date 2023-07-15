624d6b0f072f16b06d0ab104bf4641c24464ba11
morehit <95517329+morehit@users.noreply.github.com>
2023-07-15 18:57:46 +0530

    Create Search2dMatrix.cpp

---
+++ Day3/Search2dMatrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int col = matrix[0].size()-1 ; 
+
+        int row = 0 ; 
+        while (row < matrix.size() && col >= 0) {
+
+        if (matrix[row][col] == target) {
+            return true ;
+        }
+        if (matrix[row][col] < target) {
+            row++;
+        }
+        else {
+            col--;
+        }
+        }
+        return false ; 
+    }
+};
